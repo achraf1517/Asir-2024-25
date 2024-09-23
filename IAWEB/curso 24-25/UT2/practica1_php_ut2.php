62cec05db74cfc89dd1caff718737f41e74e11fb
achraf1517 <achita.thebest07@gmail.com>
2024-09-23 14:00:42 +0200

    lo de hoy

---
+++ IAWEB/curso 24-25/UT2/practica1_php_ut2.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+ <head>
+ 
+ <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+ </head>
+ <body>
+    <?php 
+    $num1=9; 
+    $num1=4;
+    $suma=$num1+$num2;
+    echo "cuando se suma el $num1 con el $num2 da como resultado $suma"; 
+    ?>
+
+ </body>
+</html>
