e40861fffba78ba3e9e623e8a469e4463e487559
Achraf1517 <Achita.thebest07@gmail.com>
2024-11-13 19:58:54 +0100

    hoy

---
+++ IAWEB_repaso/IAWEB (NO BORRAR)/UT 5/Práctica2_2.php
@@ -0,0 +1,21 @@
+<html>
+
+<head>
+
+</head>
+
+<body>
+    <?php
+        $precios = array("azúcar"=>2.7, "aceite"=>4, "arroz"=>1.5, "sal"=>0.8, "pollo"=>2.4, "tomates"=>1.3, "pan"=>0.6);
+        $total = 0;
+        foreach ($precios as $producto => $valor) {
+            if ($valor <= 2.1 and $valor >= 0.6) {
+                $total ++;
+            }
+        }
+        $porcentaje = ($total/count($precios))*100;
+        echo "Hay un porcentaje del $porcentaje % de productos con un precio comprendido entre los 0.6€ y los 2.1€.";
+    ?>
+</body>
+
+</html>
